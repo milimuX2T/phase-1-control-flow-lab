a031bd014e3cf0a54ce99f4617d5afb8f7e9fb26
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-15 22:43:35 +0300

    Lab Done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+  if(num <= 400) {
+    return 'This one is on me!'
+  }else if (num > 2500) {
+    return 'No can do.'
+  }else if (num > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+    
+
+  }
+    
+  
+    
+  
+    
+  
+  
+  
+  // Write your code here!
+
+
+function ternaryCheckCity(state){
+   return state === "NYC" ? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default :
+      return 'Bye.'  
+  }
   // Write your code here!
 }
